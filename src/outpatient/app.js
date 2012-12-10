3e212d06d45b5b1393c5e8e203b3f6d8b1bacc02
Nathan Leiby <nathanleiby@gmail.com>
2012-12-10 19:04:53 +0530

    OPD remove unused lines

--- src/outpatient/app.js
+++ src/outpatient/app.js
@@ -1,7 +1,5 @@
 Ext.Loader.setPath({
     'Ext.ux.touch.grid': '../lib/touch/Ext.ux.touch.grid',
-    //Path to grid code so that grid fuctionalities and view can be accessed
-    // 'Screener': '/Raxa-JSS/src/screener/app'//Path to screener module so that store and models of screener can be accessed 
     'Screener': '../screener/app' //Path to screener module so that store and models of screener can be accessed 
 });
 
@@ -15,7 +13,6 @@ Ext.application({
     views: ['Viewport'],
     controllers: ['patientlist', 'AddPatient'],
 
-    // views: ['Main'],
     icon: {
         '57': 'resources/icons/Icon.png',
         '72': 'resources/icons/Icon~ipad.png',
@@ -35,6 +32,7 @@ Ext.application({
     },
 
     launch: function() {
+        // Verify user is logged in
         if(Util.checkModulePrivilege('outpatient') && Util.uuidLoadedSuccessfully()){
             // Destroy the #appLoadingIndicator element
             Ext.fly('appLoadingIndicator').destroy();
