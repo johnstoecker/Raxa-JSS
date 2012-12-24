63bd8f8c4b6d8d5f1b433a95bc691a8023916511
Piyush Madan <madanpiyush@gmail.com>
2012-12-24 22:53:52 +0530

    opdLite: incorrect url in obs store (causing failure popups)

--- src/outpatient/app/store/obs.js
+++ src/outpatient/app/store/obs.js
@@ -4,7 +4,7 @@ Ext.define('RaxaEmr.Outpatient.store.obs', {
         model: 'RaxaEmr.Outpatient.model.Observation',
         proxy: {
             type: 'rest',
-            url: HOST + '/ws/rest/v1/obs?patient=85d0626e-e32e-4637-8143-2d6b600cbfe3',
+//            url: HOST + '/ws/rest/v1/obs?patient=85d0626e-e32e-4637-8143-2d6b600cbfe3',
             headers: Util.getBasicAuthHeaders(),
             reader: {
                 type: 'json',
